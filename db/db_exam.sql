9c8c7cd6c5d4def739b2bad1ae92757bd2467bfd
unknown <harishhdvp1623@gmail.com>
2022-01-09 13:16:51 +0530

    Added image and modified database

--- db/db_exam.sql
+++ db/db_exam.sql
@@ -37,7 +37,7 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`adminId`, `adminUser`, `adminPass`) VALUES
-(1, 'admin', '202cb962ac59075b964b07152d234b70');
+(1, 'admin', '12345678');
 
 -- --------------------------------------------------------
 
@@ -49,7 +49,7 @@ CREATE TABLE `tbl_ans` (
   `id` int(11) NOT NULL,
   `quesNo` int(11) NOT NULL,
   `rightAns` int(11) NOT NULL DEFAULT '0',
-  `ans` text NOT NULL
+  `ans` text NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -57,26 +57,26 @@ CREATE TABLE `tbl_ans` (
 --
 
 INSERT INTO `tbl_ans` (`id`, `quesNo`, `rightAns`, `ans`) VALUES
-(25, 1, 1, 'available'),
-(26, 1, 0, 'Ipsum'),
-(27, 1, 0, 'established'),
-(28, 1, 0, 'passages'),
-(29, 2, 0, 'distracted'),
-(30, 2, 1, 'reader'),
-(31, 2, 0, 'Lorem'),
-(32, 2, 0, 'long'),
-(33, 3, 0, 'since '),
-(34, 3, 1, '1500s'),
-(35, 3, 0, 'dummy '),
-(36, 3, 0, 'standard '),
-(37, 4, 0, 'repeat '),
-(38, 4, 0, 'preefined '),
-(39, 4, 1, 'chunks '),
-(40, 4, 0, 'necessary'),
-(41, 5, 0, 'repetition'),
-(42, 5, 0, 'always '),
-(43, 5, 0, 'Ipsum '),
-(44, 5, 1, 'therefore ');
+(25, 1, 1, 'Database application and the database '),
+(26, 1, 0, 'Data and the database '),
+(27, 1, 0, 'The user and the database application '),
+(28, 1, 0, 'Database application and SQL '),
+(29, 2, 0, 'IDMS '),
+(30, 2, 0, 'Oracle '),
+(31, 2, 0, 'dBase-II '),
+(32, 2, 1, 'R:base '),
+(33, 3, 1, 'creating and processing forms '),
+(34, 3, 0, 'creating databases '),
+(35, 3, 0, 'processing data '),
+(36, 3, 0, 'administrating databases '),
+(37, 4, 0, 'Microsofts SQL Server '),
+(38, 4, 1, 'Microsofts Access '),
+(39, 4, 0, 'IBMs DB2 '),
+(40, 4, 0, 'Oracle Corporations Oracle '),
+(41, 5, 0, 'Users '),
+(42, 5, 0, 'Database applications '),
+(43, 5, 0, 'DBMS '),
+(44, 5, 1, 'COBOL programs ');
 
 -- --------------------------------------------------------
 
@@ -95,11 +95,11 @@ CREATE TABLE `tbl_ques` (
 --
 
 INSERT INTO `tbl_ques` (`id`, `quesNo`, `ques`) VALUES
-(7, 1, 'Are there many variations of passages of Lorem Ipsum available?'),
-(8, 2, 'Is it a long established fact that a reader will be distracted?'),
-(9, 3, 'Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s.'),
-(10, 4, 'All the Lorem Ipsum generators on the Internet tend to repeat preefined chunks as necessary?'),
-(11, 5, 'he generated Lorem Ipsum is therefore always free from repetition?');
+(7, 1, 'The DBMS acts as an interface between what two components of an enterprise-class database system?'),
+(8, 2, 'Which of the following products was the first to implement true relational algebra in a PC DBMS?'),
+(9, 3, 'The following are functions of a DBMS except ________ .'),
+(10, 4, 'A DBMS that combines a DBMS and an application generator is ________ .'),
+(11, 5, 'Which of the following is not considered to be a basic element of an enterprise-class database system?');
 
 -- --------------------------------------------------------
 
@@ -132,11 +132,11 @@ INSERT INTO `tbl_user` (`userId`, `name`, `username`, `password`, `email`, `stat
 --
 
 CREATE TABLE `tbl_viva` (
-  `id` int(200) NOT NULL,
+  `id` int(200) NOT NULL ,
   `name` varchar(2000) NOT NULL,
   `email` varchar(2000) NOT NULL,
   `facebook` varchar(2000) NOT NULL,
-  `skype` varchar(2000) NOT NULL
+  `skype` varchar(2000) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -178,7 +178,7 @@ ALTER TABLE `tbl_user`
 -- Indexes for table `tbl_viva`
 --
 ALTER TABLE `tbl_viva`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),FOREIGN KEY('id') references 'tbl_user'('userId');
 
 --
 -- AUTO_INCREMENT for dumped tables
